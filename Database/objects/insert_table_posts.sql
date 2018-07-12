c027a8d3211c051a0870f1a29c1f321beb3567ee
Jefri Sibarani <jefrisibarani@gmail.com>
2018-07-12 16:35:21 +0700

    Fix kode prefix post

--- Database/objects/insert_table_posts.sql
+++ Database/objects/insert_table_posts.sql
@@ -4,7 +4,7 @@ GO
 INSERT INTO posts (name,keterangan,numberprefix) VALUES
          ( N'POST0', N'Pos layanan 1', 'A'),
 		   ( N'POST1', N'Pos layanan 2', 'B'),
-		   ( N'POST2', N'Pos layanan 3', 'B'),
+		   ( N'POST2', N'Pos layanan 3', 'C'),
 		   ( N'POST3', N'Pos layanan 4', 'D'),
 		   ( N'POST4', N'Pos layanan 5', 'BK');
 GO
