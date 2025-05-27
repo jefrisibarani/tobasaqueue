3398480ea6577e12378dc2a05d3f1b396b5b8687
Jefri Sibarani <jefrisibarani@gmail.com>
2025-05-27 14:24:14 +0700

    Save

--- QueueService/Properties/Settings.Designer.cs
+++ QueueService/Properties/Settings.Designer.cs
@@ -110,7 +110,7 @@ namespace Tobasa.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("MYSQL")]
+        [global::System.Configuration.DefaultSettingValueAttribute("MSSQL")]
         public string ProviderType {
             get {
                 return ((string)(this["ProviderType"]));
